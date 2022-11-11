f877bb2ffade5e2a7b9721a4220f30c57eada7ad
Jennifer Reuting <jen@reuting.com>
2022-11-10 16:36:09 -0800

    Added get routes for jobs and related tests

--- routes/jobs.js
+++ routes/jobs.js
@@ -40,4 +40,62 @@ const router = new express.Router();
   return res.status(201).json({ job });
 });
 
+/** GET /  =>
+ *   { jobs: [ { id, title, salary, equity, company_handle }, ...] }
+ *
+ * Authorization required: none
+ */
+
+ router.get("/", async function (req, res, next) {
+  // const filters = req.query;
+  let jobs = await Job.findAll();
+
+  // // if no query strings are passed in, call findAll()
+  // if (Object.keys(filters).length === 0) {
+  // } else {
+  //   // Convert stringified nums to nums for maxEmployees and minEmployees
+  //   if (!isNaN(Number(filters.maxEmployees))) {
+  //     filters.maxEmployees = Number(filters.maxEmployees);
+  //   }
+
+  //   if (!isNaN(Number(filters.minEmployees))) {
+  //     filters.minEmployees = Number(filters.minEmployees);
+  //   }
+
+    // Validate query strings against schema
+    // const validator = jsonschema.validate(
+    //   filters,
+    //   jobsFilterSchema,
+    //   { required: true }
+    // );
+    // if (!validator.valid) {
+    //   const errs = validator.errors.map(e => e.stack);
+    //   throw new BadRequestError(errs);
+    // }
+
+    // pass in query object to findFiltered()
+    // jobs = await Jobs.findFiltered(filters);
+  // }
+
+    return res.json({ jobs });
+
+});
+
+/** GET /[id]  =>  { job }
+ *
+ *  Job is {  id, title, salary, equity, company_handle }
+ *
+ * Authorization required: none
+ */
+
+ router.get("/:id", async function (req, res, next) {
+  const job = await Job.get(req.params.id);
+  return res.json({ job });
+});
+
+
+
+
+
+
 module.exports = router;
--- routes/jobs.test.js
+++ routes/jobs.test.js
@@ -78,3 +78,70 @@ describe("POST /jobs", function () {
     expect(resp.statusCode).toEqual(400);
   });
 });
+
+/************************************** GET ALL / Jobs */
+
+describe("GET /jobs", function () {
+  test("ok for anon", async function () {
+    const resp = await request(app).get("/jobs");
+    expect(resp.body).toEqual({
+      jobs:
+        [
+          {
+            "id": 1,
+            "title": "j1",
+            "salary": 10000,
+            "equity": "0.05",
+            "companyHandle": "c1"
+          },
+          {
+            "id": 2,
+            "title": "j2",
+            "salary": 50000,
+            "equity": "0.025",
+            "companyHandle": "c1"
+          },
+          {
+            "id": 3,
+            "title": "j3",
+            "salary": 100000,
+            "equity": "0.075",
+            "companyHandle": "c2"
+          },
+        ],
+    });
+  });
+
+  test("fails: test next() handler", async function () {
+    // there's no normal failure event which will cause this route to fail ---
+    // thus making it hard to test that the error-handler works with it. This
+    // should cause an error, all right :)
+    await db.query("DROP TABLE jobs CASCADE");
+    const resp = await request(app)
+      .get("/jobs")
+      .set("authorization", `Bearer ${u1Token}`);
+    expect(resp.statusCode).toEqual(500);
+  });
+});
+
+/************************************** GET /jobs/:id */
+
+describe("GET /jobs/:id", function () {
+  test("works for anon", async function () {
+    const resp = await request(app).get(`/jobs/1`);
+    expect(resp.body).toEqual({
+      job: {
+        "id": 1,
+        "title": "j1",
+        "salary": 10000,
+        "equity": "0.05",
+        "companyHandle": "c1"
+      },
+    });
+  });
+
+  test("not found for no such job", async function () {
+    const resp = await request(app).get(`/jobs/0`);
+    expect(resp.statusCode).toEqual(404);
+  });
+});
